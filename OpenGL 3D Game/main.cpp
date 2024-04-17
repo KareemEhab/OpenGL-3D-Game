7659b8e17b77d348e97a3401fe26a902eab70962
KareemEhab <kareem8002010@gmail.com>
2024-04-17 10:23:12 +0200

    Initialize Window

---
+++ OpenGL 3D Game/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Window Title", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Could not create window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Set context for GLAD
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialize GLAD" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Send new frame to window
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
